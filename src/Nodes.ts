e5dc117ed12fcc2cb291479aa15d3b2b2899efe6
Yan Novak <yan.novak.2014@gmail.com>
2022-06-08 09:47:54 +0300

    init sunrise-dom

---
+++ src/Nodes.ts
@@ -0,0 +1,72 @@
+import { map, Value } from '@snapview/sunrise'
+import { Property } from './Properties'
+
+export interface Component<T extends HTMLElement> {
+    readonly element: T
+}
+
+export function node<K extends keyof HTMLElementTagNameMap>(
+    tag: K,
+): (attributes: Property<HTMLElementTagNameMap[K]>[]) => HTMLElementTagNameMap[K] {
+    return function (attributes) {
+        const element = document.createElement(tag)
+        for (const attr of attributes) {
+            attr(element)
+        }
+        return element
+    }
+}
+
+export function wrap<T extends HTMLElement>(el: T): (attributes: Property<T>[]) => HTMLElement {
+    return function (attributes) {
+        for (const attr of attributes) {
+            attr(el)
+        }
+        return el
+    }
+}
+
+// TODO: put `cell` as the first argument (like in Elm)
+export function renderIf<T, E extends Node>(
+    renderFunction: ((val: T) => E) | E,
+    cell: Value<T | null>,
+): E {
+    const emptyNode = getEmptyNode() as E
+    let el = emptyNode
+
+    map((val) => {
+        /*
+         * In tests context, due to the async nature of cells, the document could not be undefined at the point of execution.
+         * We don't currently have a way to release cells, so for now we just return and skip the rest of the function.
+         */
+        if (!document) return
+
+        const newEl = val
+            ? typeof renderFunction === 'function'
+                ? renderFunction(val as T)
+                : renderFunction
+            : emptyNode
+        el.parentElement?.replaceChild(newEl, el)
+        el = newEl
+    }, cell)
+
+    return el
+}
+
+export const getEmptyNode: () => Node = () => document.createTextNode('')
+
+export const div = node('div')
+export const video = node('video')
+export const select = node('select')
+export const option = node('option')
+export const button = node('button')
+export const span = node('span')
+export const img = node('img')
+export const hr = node('hr')
+export const br = node('br')
+export const input = node('input')
+export const textarea = node('textarea')
+export const label = node('label')
+export const ul = node('ul')
+export const li = node('li')
+export const b = node('b')
